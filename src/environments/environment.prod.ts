583b179c4bdf7fd4a641bf470adbb055123605bb
Aditya Kalluraya <aditya@thewebpeople.in>
2017-12-12 13:12:42 +0530

    Angular 5 Firebase integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAQU2e4Buckup-wJs8fWqrJGMJ1_F1B2sk",
+    authDomain: "ng4fbbootstrap-2067e.firebaseapp.com",
+    databaseURL: "https://ng4fbbootstrap-2067e.firebaseio.com",
+    projectId: "ng4fbbootstrap-2067e",
+    storageBucket: "",
+    messagingSenderId: "801955198242"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAQU2e4Buckup-wJs8fWqrJGMJ1_F1B2sk",
+    authDomain: "ng4fbbootstrap-2067e.firebaseapp.com",
+    databaseURL: "https://ng4fbbootstrap-2067e.firebaseio.com",
+    projectId: "ng4fbbootstrap-2067e",
+    storageBucket: "",
+    messagingSenderId: "801955198242"
+  }
 };
